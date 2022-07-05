c8cdb381f7cedfdfe38d62a110138ab3718c8e1f
ters81 <iters81@gmail.com>
2022-07-05 19:53:36 +0100

    Add url config

---
+++ horoscope/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('aries', views.aries),
+    path('taurus', views.taurus),
+    path('gemini', views.gemini),
+    path('cancer', views.cancer),
+    path('leo', views.leo),
+    path('virgo', views.virgo),
+    path('libra', views.libra),
+    path('scorpio', views.scorpio),
+    path('sagittarius', views.sagittarius),
+    path('capricorn', views.capricorn),
+    path('aquarius', views.aquarius),
+    path('pisces', views.pisces),
+]
--- horoscope_project/urls.py
+++ horoscope_project/urls.py
@@ -14,22 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from horoscope import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('horoscope/aries', views.aries),
-    path('horoscope/taurus', views.taurus),
-    path('horoscope/gemini', views.gemini),
-    path('horoscope/cancer', views.cancer),
-    path('horoscope/leo', views.leo),
-    path('horoscope/virgo', views.virgo),
-    path('horoscope/libra', views.libra),
-    path('horoscope/scorpio', views.scorpio),
-    path('horoscope/sagittarius', views.sagittarius),
-    path('horoscope/capricorn', views.capricorn),
-    path('horoscope/aquarius', views.aquarius),
-    path('horoscope/pisces', views.pisces),
+    path('horoscope/', include('horoscope.urls')),
 ]
 
