ec018e6571d1945b9e55ca84e422142557ad2844
ters81 <iters81@gmail.com>
2022-07-08 12:29:49 +0100

    Add main page

---
+++ horoscope/urls_horoscope.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('<int:sign_zodiac>', views.get_info_about_sign_zodiac_by_number),
+    path('<str:sign_zodiac>', views.get_info_about_sign_zodiac, name='horoscope-name'),
+]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,52 +1,46 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
-def aries(request):
-    return HttpResponse("Знак зодиака - Овен")
+zodiac_dict = {
+    'aries': "Знак зодиака - Овен",
+    'taurus': "Знак зодиака - Телец",
+    'gemini': "Знак зодиака - Близнецы",
+    'cancer': "Знак зодиака - Рак",
+    'leo': "Знак зодиака - Лев",
+    'virgo': "Знак зодиака - Дева",
+    'libra': "Знак зодиака - Весы",
+    'scorpio': "Знак зодиака - Скорпион",
+    'sagittarius': "Знак зодиака - Стрелец",
+    'capricorn': "Знак зодиака - Козерог",
+    'aquarius': "Знак зодиака - Водолей",
+    'pisces': "Знак зодиака - Рыбы"
+}
 
 
-def taurus(request):
-    return HttpResponse("Знак зодиака - Телец")
+def get_info_about_sign_zodiac(request, sign_zodiac):
+    description = zodiac_dict.get(sign_zodiac, None)
+    if description:
+        return HttpResponse(f'<h2>{description}</h2>')
+    else:
+        return HttpResponseNotFound(f"Неизвестный знак зодиака - {sign_zodiac}")
 
 
-def gemini(request):
-    return HttpResponse("Знак зодиака - Близнецы")
+def get_info_about_sign_zodiac_by_number(request, sign_zodiac: int):
+    if sign_zodiac > len(zodiac_dict) or sign_zodiac == 0:
+        return HttpResponse(f"Неправильный значание номера знака Зодиака - {sign_zodiac}")
+    else:
+        zodiac_name = list(zodiac_dict)[sign_zodiac - 1]
+        redirect_url = reverse('horoscope-name', args=(zodiac_name,))
+        return HttpResponseRedirect(redirect_url)
 
 
-def cancer(request):
-    return HttpResponse("Знак зодиака - Рак")
-
-
-def leo(request):
-    return HttpResponse("Знак зодиака - Лев")
-
-
-def virgo(request):
-    return HttpResponse("Знак зодиака - Дева")
-
-
-def libra(request):
-    return HttpResponse("Знак зодиака - Весы")
-
-
-def scorpio(request):
-    return HttpResponse("Знак зодиака - Скорпион")
-
-
-def sagittarius(request):
-    return HttpResponse("Знак зодиака - Стрелец")
-
-
-def capricorn(request):
-    return HttpResponse("Знак зодиака - Козерог")
-
-
-def aquarius(request):
-    return HttpResponse("Знак зодиака - Водолей")
-
-
-def pisces(request):
-    return HttpResponse("Знак зодиака - Рыбы")
+def index(request):
+    li_elements = ''
+    for sign in zodiac_dict:
+        redirect_path = reverse('horoscope-name', args=(sign,))
+        li_elements += f"<li> <a href='{redirect_path}'> {sign.capitalize()} </a> </li>"
+    response = f'<ul>{li_elements}</ul>'
+    return HttpResponse(response)
